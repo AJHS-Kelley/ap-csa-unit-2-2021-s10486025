a3f1daa67ff024b0d7a41cfb8a6d615ae6b496db
Ty W <71028905+s10486025@users.noreply.github.com>
2021-11-03 08:45:07 -0400

    v0.0

---
+++ student.java
@@ -0,0 +1,37 @@
+public class student {
+
+public static void calcGPA() {
+    int grade0;
+    int grade1;
+    int grade2;
+    int grade3;
+    double newGPA;
+
+    import java.until.Scanner
+    Scanner myScanner = new Scanner(System.in);
+
+    System.out.println("What is the first grade?");
+    grade0= myScanner.nextInt();
+    System.out.println("What is the second grade?");
+    grade1= myScanner.nextInt();
+    System.out.println("What is the third grade?");
+    grade2= myScanner.nextInt();
+    System.out.println("What is the fourth grade?");
+    grade3= myScanner.nextInt();
+
+
+
+
+
+
+
+
+}
+
+
+}
+
+
+
+
+
